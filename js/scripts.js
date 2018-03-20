aa0a3dc08dc356d3000c2a2fa6e77fe4c255d84d
Garnett Puett and Ted Leary <ted@>
2018-03-20 10:09:46 -0700

    Add day/night modes and border/highlight for text.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 $(document).ready(function() {
-  $("p").click(function() {
+  $(".clickable").click(function() {
     $(".test1").fadeToggle();
     $(".test2").slideToggle();
   });
+  $("button#night").click(function() {
+    $("body").removeClass();
+    $("body").addClass("night");
+  });
+  $("button#day").click(function() {
+    $("body").removeClass();
+    $("body").addClass("day");
+  });
+  $(".highlight").click(function(){
+    $(".highlight2").removeClass("box");
+    $(".highlight2").addClass("box");
+  })
 });
